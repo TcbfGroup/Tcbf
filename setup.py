05645ab2916090f85999a67f2a770028ecd8af2b
xinh <hexin9808@gmail.com>
2022-11-18 14:11:24 +0800

    add plot

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ setup(name = "tcbf",
       license_files = ('LICENSE',),
       packages = ["tcbf"],
       url = "https://github.com/hexin010101/Tcbf",
-     scripts=['bin/tcbf','bin/tcbf_syn_process'])
+     scripts=['bin/tcbf','bin/tcbf_syn_process','bin/plot_TAD_bound_synteny'])
