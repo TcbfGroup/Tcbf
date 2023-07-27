03ab19b1fc39814f12b997095c0a8ce118f9f31a
HeXin <1968779700@qq.com>
2023-07-27 15:10:55 +0800

    change lastz parameters

--- tcbf/aligner_paramters.py
+++ tcbf/aligner_paramters.py
@@ -62,33 +62,32 @@ def parallel_lastz(query,target,paramters,threads):
                 result = read_table(result_tmp.name)
                 return result
 
-    from concurrent.futures import ProcessPoolExecutor,ThreadPoolExecutor
+    from concurrent.futures import ThreadPoolExecutor
     target_file = pysam.FastaFile(target)
     sequences = {i:target_file[i] for i in target_file.references}
     with ThreadPoolExecutor(max_workers=threads)as executor:
-        results = [result for result in executor.map(run_lastz, sequences)]
+        results = list(executor.map(run_lastz, sequences.keys()))
         results = concat(results)
     return results
-
-
-
-
-
 
 def lastz_align(workdir,bound_query, target, output_file, query,threads,map_length = 50, parameter=None):
 
     if not parameter:
-        parameter = f"E=30 H=3000 K=5000 L=5000 M=10 O=400 T=1 Q={os.path.join(workdir,'Step2','general.q')} --notransition "
-    parameter += " --ambiguous=iupac    --format=general:name1,start1,end1,name2,start2,end2  "
-    general_q = """A C G T
-91 -114 -31 -123
--114 100 -125 -31
--31 -125 100 -114
--123 -31 -114 91"""
-    with open(os.path.join(workdir,"Step2","general.q"),"w")as f:
-        f.write(general_q)
-    result = parallel_lastz(bound_query,target,parameter,threads)
-    result.to_csv(output_file,index=False)
+#         parameter = f"E=30 H=3000 K=5000 L=5000 M=10 O=400 T=1 Q={os.path.join(workdir,'Step2','general.q')} --notransition --step=20"
+#     parameter += " --ambiguous=iupac    --format=general:name1,start1,end1,name2,start2,end2  "
+#     general_q = """A C G T
+# 91 -114 -31 -123
+# -114 100 -125 -31
+# -31 -125 100 -114
+# -123 -31 -114 91"""
+
+        # with open(os.path.join(workdir,"Step2","general.q"),"w")as f:
+        #     f.write(general_q)
+        parameter = " --notransition --step=20  --format=general:name1,start1,end1,name2,start2,end2  --ambiguous=iupac  --nogapped "
+    results = parallel_lastz(bound_query,target,parameter,threads)
+    results.index =  "seq_id start end seq_id2 start2 end2".split()
+
+    results.to_csv(output_file,index=False)
 
 def last_align(workdir,bound_query, target, output_file,threads,map_length = 50):
     names = "queryid subjectid  identity alignment_length mismatches gapopens " \
@@ -105,7 +104,7 @@ def last_align(workdir,bound_query, target, output_file,threads,map_length = 50)
                 if not os.path.exists(os.path.join(db_dir,f"{seq_id}.prj")):
                     seq = sequences[seq_id]
                     fasta_tmp_file.write(f">{seq_id}\n{format_seq(seq)}")
-                    command1 = f"lastdb {os.path.join(db_dir,seq_id)} {fasta_tmp_file.name};"
+                    command1 = f"lastdb -uMAM4 {os.path.join(db_dir,seq_id)} {fasta_tmp_file.name};"
                     run_command(command1)
 
 
